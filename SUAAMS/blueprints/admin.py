4866a8a7f2e980768a6e2ebf6bdc7e11eac70cfd
Afeez1234 <90616642+Afeez1234@users.noreply.github.com>
2026-06-17 15:45:11 +0100

    moving on to the create student endpoint

--- SUAAMS/blueprints/admin.py
+++ SUAAMS/blueprints/admin.py
@@ -1,6 +1,7 @@
 from flask import Blueprint,flash,render_template,redirect,request,session,url_for
 from utils import login_required
 from utils import connect_to_database
+import bcrypt
 admin_bp = Blueprint('admin', __name__)
 
 @admin_bp.route('/admin/dashboard')
@@ -35,3 +36,23 @@ def create_lecturer():
             connection.close()
             return render_template('admin/create-lecturer.html')
         
+        password = staff_id
+        password_bytes = password.encode('utf-8')
+        hashed_password = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
+        try:
+            cursor.execute("INSERT INTO users (username, passwordhash, role, is_active) VALUES (%s, %s, %s, %s)", 
+                        (staff_id, hashed_password, 'lecturer', 1))
+            user_id = cursor.lastrowid
+            cursor.execute("INSERT INTO lecturers (user_id, full_name, staff_id) VALUES (%s, %s, %s)", 
+                        (user_id, full_name))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            flash('Lecturer created successfully.', 'success')
+            return redirect(url_for('admin.create_lecturer'))
+        except Exception as e:
+            connection.rollback()
+            cursor.close()
+            connection.close()
+            flash('Error occurred while creating lecturer.', 'error')
+            return redirect(url_for('admin.create_lecturer'))
