ddfa603a25ca179a422a219aed04a310cefcf68f
Afeez1234 <90616642+Afeez1234@users.noreply.github.com>
2026-06-17 08:06:24 +0100

    i removed them since i copied it outside and made changes

--- SUAAMS/suaams_Api/config.py
+++
@@ -1,9 +0,0 @@
-DB_CONFIG = {
-    'host': 'localhost',
-    'user': 'root',
-    'password': 'Aleshinloye2006#',
-    'database': 'suaams'
-}
-
-SERIAL_PORT = 'COM6'  # Update this to your actual serial port
-BAUD_RATE = 9600
